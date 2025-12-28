f39c1d211e7b4aef2b1bf60fc3163eb7549b5a68
Gonroz <wasabaininja@gmail.com>
2025-12-27 21:53:59 -0500

    git init
    First commit of basic code.

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use dioxus::prelude::*;
+
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const MAIN_CSS: Asset = asset!("/assets/main.css");
+const HEADER_SVG: Asset = asset!("/assets/header.svg");
+
+fn main() {
+    dioxus::launch(App);
+}
+
+#[derive(Routable, Clone, PartialEq)]
+enum Route {
+    #[route("/")]
+    RenderArea,
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        NavBar {}
+        RenderArea {}
+    }
+}
+
+#[component]
+pub fn RenderArea() -> Element {
+    rsx! {
+        div { class: "renderarea",
+            "Hello Dioxus!"
+        }
+    }
+}
+
+#[component]
+pub fn NavBar() -> Element {
+    rsx! {
+        div { class: "container",
+        div { class: "navbar",
+            ul {
+                li { class: "nav-item", "Order" }
+                li { class: "nav-item", "Order" }
+                li { class: "nav-item", "Order" }
+                li { class: "nav-item", "Order" }
+                li { class: "nav-item", "Order" }
+                li { class: "nav-item", "Order" }
+            }
+        }
+        }
+    }
+}
