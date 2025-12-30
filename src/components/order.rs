50652d2734e29dd37a445f307dae76c0de1d5a53
Gonroz <wasabaininja@gmail.com>
2025-12-30 17:27:56 -0500

    order screen sizing work
    The order screen now seems to be organized properly to see the toppings
    and the sizes in the middle, then have the current order container on
    the righthand side.

--- src/components/order.rs
+++ src/components/order.rs
@@ -4,20 +4,25 @@ use dioxus::prelude::*;
 pub fn Order() -> Element {
     rsx! {
         div { class: "order-container",
-            div { class: "sizes",
-                button { "Personal" }
-                button { "Small" }
-                button { "Large" }
-                button { "Sheet" }
-            }
-            div { class: "toppings",
-                button { "Pepperoni" }
-                button { "Onions" }
-                button { "Olives" }
-                button { "Spinach" }
-                for _ in 0..8 {
-                    button { "blah" }
+            div { class: "order-options",
+                div { class: "sizes",
+                    button { "Personal" }
+                    button { "Small" }
+                    button { "Large" }
+                    button { "Sheet" }
                 }
+                div { class: "toppings",
+                    button { "Pepperoni" }
+                    button { "Onions" }
+                    button { "Olives" }
+                    button { "Spinach" }
+                    for _ in 0..8 {
+                        button { "blah" }
+                    }
+                }
+            } // order options bracket
+            div { class: "current-order-container",
+                "Hello there! General Kenobi!"
             }
         }
     }
