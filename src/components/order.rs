54787a7b2f90051389c14a2840135609f0b7e4be
Gonroz <wasabaininja@gmail.com>
2026-01-06 23:40:16 -0500

    Basic pizza functionality for order system
    Incredibly basic pizzas now show up in the current order area. There is
    no way to create more other than with code right now.

--- src/components/order.rs
+++ src/components/order.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 use dioxus::prelude::*;
 
 #[component]
@@ -12,7 +14,12 @@ pub fn Order() -> Element {
                     button { "Sheet" }
                 }
                 div { class: "toppings",
-                    button { "Pepperoni" }
+                    button {
+                        // onclick: move |_| {
+                        //     let _ = Pizza();
+                        // },
+                        "Pepperoni"
+                    }
                     button { "Onions" }
                     button { "Olives" }
                     button { "Spinach" }
@@ -22,13 +29,92 @@ pub fn Order() -> Element {
                 }
             } // order options bracket
             div { class: "current-order-container",
-                div { class: "current-order",
-                    "current order"
-                }
+                CurrentOrder {}
                 button { class: "send-to-kitchen",
                     "Send to Kitchen"
                 }
             }
+        }
+    }
+}
+
+// #[derive(PartialEq, Clone, Props)]
+// struct PizzaProps {
+//     size: u8,
+//     toppings: u8,
+// }
+
+#[derive(PartialEq, Clone, Copy)]
+pub enum PizzaSize {
+    Small,
+    Large,
+}
+
+impl fmt::Display for PizzaSize {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            PizzaSize::Small => write!(f, "Small"),
+            PizzaSize::Large => write!(f, "Large"),
+        }
+    }
+}
+
+#[derive(PartialEq, Clone, Copy)]
+pub enum PizzaTopping {
+    Pepperoni,
+    Onion,
+}
+
+impl fmt::Display for PizzaTopping {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            PizzaTopping::Pepperoni => write!(f, "Pepperoni"),
+            PizzaTopping::Onion => write!(f, "Onion"),
+        }
+    }
+}
+
+#[component]
+pub fn PizzaDiv(size: PizzaSize, topping: PizzaTopping) -> Element {
+    let mut size = use_signal(|| PizzaSize::Small);
+    let mut topping = use_signal(|| PizzaTopping::Pepperoni);
+
+    rsx! {
+        div {
+            "Size: {size} -- Toppings: {topping}"
+        }
+    }
+}
+
+struct Pizza {
+    size: PizzaSize,
+    topping: PizzaTopping,
+}
+
+// impl fmt::Display for Pizza {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         write!(f, format!("Size: {self.size} Topping: {self.topping}"))
+//     }
+// }
+
+#[component]
+pub fn CurrentOrder() -> Element {
+    let mut pizzas: Signal<Vec<Pizza>> = use_signal(|| vec![]);
+    pizzas.push(Pizza {
+        size: PizzaSize::Small,
+        topping: PizzaTopping::Pepperoni,
+    });
+    pizzas.push(Pizza {
+        size: PizzaSize::Large,
+        topping: PizzaTopping::Onion,
+    });
+
+    rsx! {
+        div { class: "current-order",
+            for p in pizzas.iter() {
+                PizzaDiv { size: p.size, topping: p.topping }
+            }
+            "argh"
         }
     }
 }
