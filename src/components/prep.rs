a758d6b56a22752638fb673a0225f0096d5651b5
Gonroz <wasabaininja@gmail.com>
2026-01-30 01:46:23 -0500

    styling: pizzas in prep now size properly
    Pizzas in prep now size properly and can be scrolled along right

--- src/components/prep.rs
+++ src/components/prep.rs
@@ -23,7 +23,7 @@ pub fn Prep() -> Element {
     use_context_provider(|| PrepContext::new());
 
     rsx! {
-        div { class: "order-container",
+        div { class: "prep-container",
             PrepStation { }
         }
     }
@@ -66,7 +66,7 @@ pub fn PizzaPrep(pizza: pizza::Pizza) -> Element {
 
     rsx! {
         div { class: "pizza-prep",
-            "pizza"
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         }
     }
 }
